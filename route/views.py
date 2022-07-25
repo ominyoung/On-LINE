d87246f13d08720af8b998c3d2792ac5e4c47fd2
ominyoung <ohmj090747@gmail.com>
2022-07-25 23:30:08 +0900

    기상청 단기 api

--- route/views.py
+++ route/views.py
@@ -11,6 +11,7 @@ from .models import ResultModel, MemoModel, PlanModel
 
 
 def index(request):
+    # 기상청 중기 api
     # 현재 날짜 시간을 0600, 1800만 받아올수 있음 예시) 202207190600
     dt_now = datetime.now()
     if dt_now.hour < 6:
@@ -22,8 +23,6 @@ def index(request):
     else:
         dt_aj = datetime.strftime(dt_now, '%Y-%m-%d')
         tmFc = str(datetime.strftime(dt_now, '%Y%m%d'))+"1800"
-    #  + dt_now.day
-    # 기상청 api
     url = 'http://apis.data.go.kr/1360000/MidFcstInfoService/getMidLandFcst'
     params = {'serviceKey': 'bWG0fEz3JXziaxVbvDmZy9L97AdOHLEF7FrSPGynfDVMsrYAFF5NjMWTgSnhS2I70i7ziWy6QWPMxdQ3eSlLFA==',
               'pageNo': '1',
@@ -31,12 +30,49 @@ def index(request):
               'dataType': 'JSON',
               'regId': '11G00000', # 제주도
               'tmFc': tmFc}
+
+    # 기상청 단기 api
+    dt_short_now = datetime.now()
+    if dt_short_now.hour < 5:
+        dt_short_aj = str(datetime.strftime(dt_short_now - datetime.timedelta(1), '%Y%m%d'))
+    else:
+        dt_short_aj = str(datetime.strftime(dt_short_now, '%Y%m%d'))
+
+    url_short = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
+    params_short = {'serviceKey': 'bWG0fEz3JXziaxVbvDmZy9L97AdOHLEF7FrSPGynfDVMsrYAFF5NjMWTgSnhS2I70i7ziWy6QWPMxdQ3eSlLFA==',
+              'pageNo': '1', 'numOfRows': '1000', 'dataType': 'JSON', 'base_date': dt_short_aj, 'base_time': '0500',
+              'nx': '53', 'ny': '38'}  # nx, ny : 제주도 일도동,이도동
+
     # tmFc에 현재 날짜 str로 더해서 넣기
     response = requests.get(url, params=params)
+    response_short = requests.get(url_short, params=params_short)
+    items = response_short.json().get('response').get('body').get('items').get('item')
+
+    sky = []
+    pty = []
+    sky_dict = {}
+    pty_dict = {}
+    for i in items:
+        if i["category"] == "SKY":
+            if i["fcstTime"] == '0600' or i["fcstTime"] == "1200" or i["fcstTime"] == "1800":
+                datestring = datetime.strptime(i["fcstDate"], "%Y%m%d")
+                sky_dict["fcstDate"] = datetime.strftime(datestring, "%Y/%m/%d")
+                sky_dict["fcstTime"] = i["fcstTime"]
+                sky_dict["fcstValue"] = i["fcstValue"]
+                sky.append(sky_dict)
+        if i["category"] == "PTY":
+            if i["fcstTime"] == '0600' or i["fcstTime"] == "1200" or i["fcstTime"] == "1800":
+                pty_dict["fcstDate"] = i["fcstDate"]
+                pty_dict["fcstTime"] = i["fcstTime"]
+                pty_dict["fcstValue"] = i["fcstValue"]
+                pty.append(pty_dict)
+
     # 3일뒤 오전 날씨
     return render(request, 'route/result.html',
-                  {'time': dt_aj,
-                   'weather': response.json().get('response').get('body').get('items').get('item')[0]
+                  {'time': dt_aj,    # 중기 api 날짜
+                   'weather': response.json().get('response').get('body').get('items').get('item')[0],  # 중기 api 날짜
+                   'sky': sky,       # 단기 api 날씨 [{날짜, 시간, 날씨}]
+                   'pty': pty,       # 단기 api 날씨 [{날짜, 시간, 강수}]
                    })
 
 
