c507015e4aeb94e9f3d594fd6875179087c15db0
ominyoung <ohmj090747@gmail.com>
2022-09-14 13:17:49 +0900

    배포전 allowed_host 변경

--- online/settings.py
+++ online/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.125.122.165']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
