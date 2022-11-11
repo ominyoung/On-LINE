43ec56ca8cd19d42cffdfaf17d1e1cc5a883ca65
ominyoung <ohmj090747@gmail.com>
2022-11-11 23:46:30 +0900

    html template tag 요일 추가

--- route/templatetags/route_tags.py
+++ route/templatetags/route_tags.py
@@ -7,9 +7,9 @@ register = template.Library()
 def route_date(startdate, day):
     startdate = list(map(int, startdate.split('-')))
     date = datetime.datetime(startdate[0], startdate[1], startdate[2])
-    arr = []
+    days = ['월요일','화요일','수요일','목요일','금요일','토요일','일요일']
+    arr = [] #날짜 담을 리스트
     for i in range(len(day)):
         today = date + datetime.timedelta(days=i)
-        arr.append(today.strftime('%Y-%m-%d'))
-    print(arr)
+        arr.append(today.strftime('%Y-%m-%d')+" "+days[today.weekday()])
     return arr
